32ff2f5816e9b3fccba6b30f986f5cb558e34451
Vikrant Shroti <vikrantshroti@gmail.com>
2018-10-25 15:37:55 +0530

    Add files via upload

---
+++ src/components/LibraryList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { ListView, View } from 'react-native';
+import ListItem from './ListItem';
+
+class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 != r2,
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow(library) {
+    return <ListItem library={library} />;
+  }
+
+  render() {
+    return (
+      <View style={{ height: 400, marginTop: 20 }}>
+        <ListView dataSource={this.dataSource} renderRow={this.renderRow} />
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
